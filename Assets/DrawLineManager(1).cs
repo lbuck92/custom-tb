e60935ecd3d35fca035a089e35bd9f1826a6cb3b
Lauren E. Buck <lauren.e.buck.1@vanderbilt.edu>
2017-07-13 12:30:05 -0500

    adding files

---
+++ Assets/DrawLineManager(1).cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLineManager : MonoBehaviour {
+
+	public Material lMat;
+
+	public SteamVR_TrackedObject trackedObj;
+
+	private MeshLineRenderer currLine;
+
+	private int numClicks = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+		if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			GameObject go = new GameObject ();
+			go.AddComponent<MeshFilter> ();
+			go.AddComponent<MeshRenderer> ();
+			currLine = go.AddComponent<MeshLineRenderer> ();
+
+			currLine.lmat = new Material (lMat);
+			currLine.setWidth (.1f);
+
+		} else if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			//currLine.SetVertexCount (numClicks + 1);
+			//currLine.SetPosition (numClicks, trackedObj.transform.position);
+
+			currLine.AddPoint (trackedObj.transform.position);
+			numClicks++;
+		} else if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			numClicks = 0;
+			currLine = null;
+		}
+
+		if (currLine != null) {
+			currLine.lmat.color = ColorManager.Instance.GetCurrentColor ();
+		}
+
+	}
+}
---
+++ Assets/Simple Color Picker/Scripts/Draggable.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+	public SteamVR_TrackedObject trackedObj;
+
+	public bool fixX;
+	public bool fixY;
+	public Transform thumb;	
+	bool dragging;
+
+	void FixedUpdate()
+	{
+		SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+		if (device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			dragging = false;
+			Ray ray = new Ray (trackedObj.transform.position, trackedObj.transform.forward);
+			//var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity)) {
+				dragging = true;
+			}
+		}
+
+		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) dragging = false;
+		if (dragging && device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			Ray ray = new Ray (trackedObj.transform.position, trackedObj.transform.forward);
+			RaycastHit hit;
+			if (GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity)) {
+				var point = hit.point;
+				point = GetComponent<Collider>().ClosestPointOnBounds(point);
+				SetThumbPosition(point);
+				SendMessage("OnDrag", Vector3.one - (thumb.position - GetComponent<Collider>().bounds.min) / GetComponent<Collider>().bounds.size.x);
+			}
+	
+
+		}
+	}
+
+	void SetDragPoint(Vector3 point)
+	{
+		point = (Vector3.one - point) * GetComponent<Collider>().bounds.size.x + GetComponent<Collider>().bounds.min;
+		SetThumbPosition(point);
+	}
+
+	void SetThumbPosition(Vector3 point)
+	{
+		thumb.position = new Vector3(fixX ? thumb.position.x : point.x, fixY ? thumb.position.y : point.y, thumb.position.z);
+	}
+}
